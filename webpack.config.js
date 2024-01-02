57c0e08adaffef3ed054cc0b3eab8f8eafd574ff
Nikita <prey122@gmail.com>
2024-01-02 12:42:38 +0000

    single config object #002

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin") 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
 
 // module.exports = {
 //   entry: {
@@ -22,47 +23,61 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 //     new CleanWebpackPlugin(),
 //     new HtmlWebpackPlugin({
 //       template: './src/index.html',
-//       chunks: ['[name]'],
-//       filename: '[name].html'
+//       chunks: 'main',
+//       filename: 'main.html'
 //     }),
-//     // new HtmlWebpackPlugin({
-//     //   template: './src/page1.html',
-//     //   chunks: ['page1'],
-//     //   filename: 'page1.html'
-//     // }),
+//     new HtmlWebpackPlugin({
+//       template: './src/page1.html',
+//       chunks: 'page1',
+//       filename: 'page1.html'
+//     }),
 //   ]
 // };
 
+function collectorPlugins(htmls, difrentPlugins, objCongig) {
+  const arrHtmlPlugins = htmls.map(item => new HtmlWebpackPlugin({
+    template: `./src/${item}.html`,
+    chunks: item === 'index' ? ['main'] : ['main', ...htmls.filter(item => item !== 'index')],
+    filename: `${item}.html`
+  }))
+
+  objCongig.plugins = [...arrHtmlPlugins, ...difrentPlugins];
+}
+
 module.exports = (env, argv) => {
-
-  function getConfig (nameFile, isClean) {
-    const entryObj = {}
-    
-    nameFile.forEach((item => entryObj[item] = `./src/${item}.js`))
-
-    return {
-      entry: entryObj,
-      output: {
-        filename: `[name].js`,
-        path: __dirname + '/dist'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-          }
-        ]
-      },
-      plugins: [
-        isClean && new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-          template: `./src/${nameFile[nameFile.length - 1]}.html`,
-          filename: `${nameFile[nameFile.length - 1]}.html`
-        })
+  const config = {
+    entry: {
+      main:"./src/index.js",
+      page1:"./src/page1.js",
+    },
+    devServer: {
+      static: path.resolve(__dirname, './dist'),
+      compress: true,
+      hot: true,
+      port: 8080,
+      open: true
+    },
+    mode: 'development',
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/dist'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+          type: 'asset/resource'
+        },
       ]
-    }
+    },
+    plugins: [] // don't put here plugins, use function below
   }
 
-  return [getConfig(['index'], true), getConfig(['index', 'page1'])]
+  collectorPlugins(['index', 'page1'], [new CleanWebpackPlugin()], config)
+
+  return config;
 }
